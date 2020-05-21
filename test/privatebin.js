a95599a945a9716d7de23a1e77a1948eca13b8f8
Amine Benseddik <amine.benseddik@gmail.com>
2020-05-21 11:16:36 +0200

    Add more tests (#14)
    * handle errors correctly
    
    * add more tests

---
+++ test/privatebin.js
@@ -0,0 +1,18 @@
+import tap from 'tap';
+import nock from 'nock';
+import {
+  host, response, pasteDataBuf, randomKey, opts, paste,
+} from './mock';
+
+import privatebin from '../lib/privatebin';
+
+tap.test('Should return a paste object', async (t) => {
+  nock(host).post('/').reply(200, response);
+  t.same(paste, await privatebin(host, pasteDataBuf, randomKey, opts));
+});
+
+
+tap.test('Should reject', async (t) => {
+  nock(host).post('/').reply(404, {});
+  t.rejects(privatebin(host, pasteDataBuf, randomKey, opts), {});
+});
