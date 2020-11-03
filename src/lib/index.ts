64e9379324617c2e2b5322fd7bed2838a9280361
Amine Benseddik <amine@qonto.eu>
2020-11-03 23:00:27 +0100

    rename message to text

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,16 +5,16 @@ import { Api } from './api';
 import { PasteData, Paste, Response, Options, Spec, Adata } from './types';
 import { decrypt, encrypt } from './crypto';
 
-export function encryptMessage(message: Buffer, key: Buffer, spec: Spec): PasteData {
+export function encryptText(message: Buffer, key: Buffer, spec: Spec): PasteData {
   return encrypt(message, key, spec);
 }
 
-export function decryptMessage(message: string, key: Buffer, adata: Adata): Buffer {
+export function decryptText(message: string, key: Buffer, adata: Adata): Buffer {
   return decrypt(message, key, adata);
 }
 
-export function getBufferPaste(data: string, compression?: string): Buffer | Uint8Array {
-  const buf = Buffer.from(JSON.stringify({ paste: data }), 'utf8');
+export function textToBuffer(text: string, compression?: string): Buffer | Uint8Array {
+  const buf = Buffer.from(JSON.stringify({ paste: text }), 'utf8');
   if (compression === 'zlib') {
     return pako.deflateRaw(new Uint8Array(buf));
   } else {
@@ -50,18 +50,18 @@ export class PrivatebinClient extends Api {
     super(apiConfig);
   }
 
-  public async sendText(message: string, key: Buffer, options: Options): Promise<Response> {
+  public async sendText(text: string, key: Buffer, options: Options): Promise<Response> {
     const { burnafterreading, opendiscussion, compression } = options;
     const spec = getSpec(burnafterreading, opendiscussion, compression);
-    const pasteData = getBufferPaste(message, compression);
-    const chiperPaste = encryptMessage(Buffer.from(pasteData), key, spec);
+    const pasteData = textToBuffer(text, compression);
+    const chiperPaste = encryptText(Buffer.from(pasteData), key, spec);
 
     return this.postPaste(chiperPaste, options);
   }
 
   public async getText(id: string, key: Buffer): Promise<Paste> {
     const response = await this.getPaste(id);
-    const paste = decryptMessage(response.ct, key, response.adata);
+    const paste = decryptText(response.ct, key, response.adata);
 
     if (response.adata[0][7] === 'zlib') {
       return JSON.parse(pako.inflateRaw(paste, { to: 'string' }));
