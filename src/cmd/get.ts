9f26b51aa7f4a539f0636b210ab58ea369a1352c
Amine Benseddik <amine@pixelfactory.io>
2021-03-04 19:54:32 +0100

    chore: refact GetCmd into Class

--- src/cmd/get.ts
+++ src/cmd/get.ts
@@ -5,39 +5,42 @@ import { PrivatebinClient } from '../lib';
 import { PrivatebinPaste } from '../lib';
 import { concatUint8Array, stringToUint8Array } from '../lib/crypto';
 import { readPassword } from './utils';
+export class GetCmd extends commander.Command {
+  constructor() {
+    super('get');
+    this.arguments('<url>');
+    this.description('Get a text from privatebin');
+    this.option('-p, --password', 'prompt for password', false);
+    this.action(this.run);
+  }
 
-export async function getCmdAction(pasteUrl: string, password: string): Promise<PrivatebinPaste> {
-  const u = new URL(pasteUrl);
-  const id = u.search.substring(1);
-  const key = u.hash.substring(1);
-  const passPhrase = concatUint8Array(decode(key), stringToUint8Array(password));
-  const privatebin = new PrivatebinClient(u.origin);
-  return await privatebin.getText(id, passPhrase);
-}
+  private getCmdAction = async (pasteUrl: string, password: string): Promise<PrivatebinPaste> => {
+    const u = new URL(pasteUrl);
+    const id = u.search.substring(1);
+    const key = u.hash.substring(1);
+    const passPhrase = concatUint8Array(decode(key), stringToUint8Array(password));
+    const privatebin = new PrivatebinClient(u.origin);
+    return await privatebin.getText(id, passPhrase);
+  };
 
-export function NewGetCmd(): commander.Command {
-  const cmd = commander.command('get <url>');
-  cmd.option('-p, --password', 'prompt for password', false);
+  private print = (paste: PrivatebinPaste) => {
+    process.stderr.write(`${paste.paste}\n`);
+  };
 
-  cmd.description('Get a text from privatebin').action(async (url, args) => {
-    let paste: PrivatebinPaste = { paste: '' };
+  public run = async (url: string, args: any): Promise<void> => {
     let password = '';
-
     if (args.password) {
       password = await readPassword();
     }
 
     try {
-      paste = await getCmdAction(url, password);
+      this.print(await this.getCmdAction(url, password));
     } catch (err) {
       if (err.message === 'Unsupported state or unable to authenticate data') {
-        password = await readPassword();
-        paste = await getCmdAction(url, password);
+        this.print(await this.getCmdAction(url, await readPassword()));
       } else {
         throw err;
       }
     }
-    process.stderr.write(`${paste.paste}\n`);
-  });
-  return cmd;
+  };
 }
