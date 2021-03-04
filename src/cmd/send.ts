6cf7261edae01aaca75a7267aa6b879720aec2f8
Amine Benseddik <amine@pixelfactory.io>
2021-03-04 20:36:36 +0100

    chore: refact SendCmd into Class

--- src/cmd/send.ts
+++ src/cmd/send.ts
@@ -9,93 +9,84 @@ import { concatUint8Array, stringToUint8Array } from '../lib/crypto';
 import { PrivatebinClient } from '../lib/privatebin';
 import { PrivatebinResponse, PrivatebinOutput, PrivatebinOptions } from '../lib/types';
 
-function formatResponse(response: PrivatebinResponse, host: string, randomKey: Uint8Array): PrivatebinOutput {
-  return {
-    pasteId: response.id,
-    pasteURL: `${host}${response.url}#${encode(randomKey)}`,
-    deleteURL: `${host}/?pasteid=${response.id}&deletetoken=${response.deletetoken}`,
+export class SendCmd extends commander.Command {
+  constructor() {
+    super('send');
+    this.arguments('send <text>');
+    this.description('Send a text to privatebin');
+
+    const options = [
+      new commander.Option('-e, --expire <string>', 'paste expire time')
+        .default('1week')
+        .choices(['5min', '10min', '1hour', '1day', '1week', '1month', '1year', 'never']),
+      new commander.Option('--burnafterreading', 'burn after reading').default(false),
+      new commander.Option('--opendiscussion', 'open discussion').default(false),
+      new commander.Option('--compression <string>', 'use compression').choices(['zlib', 'none']).default('zlib'),
+      new commander.Option('--textformat <string>', 'text format')
+        .default('plaintext')
+        .choices(['plaintext', 'markdown']),
+      new commander.Option('-p, --password', 'prompt for password').default(false),
+      new commander.Option('-u, --url <string>', 'privateBin host').default('https://privatebin.net'),
+      new commander.Option('-o, --output <string>', 'output format').default('text').choices(['text', 'json', 'yaml']),
+    ];
+
+    options.forEach((option) => this.addOption(option));
+    this.action(this.run);
+  }
+
+  private sendCmdAction = async (
+    text: string,
+    key: Uint8Array,
+    url: string,
+    options: PrivatebinOptions,
+  ): Promise<PrivatebinResponse> => {
+    const privatebin = new PrivatebinClient(url);
+    return await privatebin.sendText(text, key, options);
   };
-}
 
-async function sendCmdAction(
-  text: string,
-  key: Uint8Array,
-  url: string,
-  options: PrivatebinOptions,
-): Promise<PrivatebinResponse> {
-  const privatebin = new PrivatebinClient(url);
-  return await privatebin.sendText(text, key, options);
-}
+  private formatResponse = (response: PrivatebinResponse, host: string, randomKey: Uint8Array): PrivatebinOutput => {
+    return {
+      pasteId: response.id,
+      pasteURL: `${host}${response.url}#${encode(randomKey)}`,
+      deleteURL: `${host}/?pasteid=${response.id}&deletetoken=${response.deletetoken}`,
+    };
+  };
 
-function validateExpire(val: string): string {
-  if (val.match(/^(5min|10min|1hour|1day|1week|1month|1year|never)$/i)) {
-    return val;
-  }
-  throw new Error(`invalid expire: ${val}`);
-}
+  public run = async (text: string, args: any): Promise<void> => {
+    if (args.burnafterreading && args.opendiscussion) {
+      throw new Error("You can't use --opendiscussion with --burnafterreading flag");
+    }
 
-function validateOutput(val: string): string {
-  if (val.match(/^(text|json|yaml)$/i)) {
-    return val;
-  }
-  throw new Error(`invalid output: ${val}`);
-}
+    let password = '';
+    if (args.password) {
+      password = await readPassword();
+    }
 
-export function NewSendCmd(): commander.Command {
-  const cmd = commander.command('send <text>');
+    const randomKey = crypto.getRandomValues(new Uint8Array(32));
+    const passPhrase = concatUint8Array(randomKey, stringToUint8Array(password));
 
-  cmd
-    .description('Send a text to privatebin')
-    .option(
-      '-e, --expire <string>',
-      'paste expire time [5min, 10min, 1hour, 1day, 1week, 1month, 1year, never]',
-      validateExpire,
-      '1week',
-    )
-    .option('--burnafterreading', 'burn after reading', false)
-    .option('--opendiscussion', 'open discussion', false)
-    .option('--compression <string>', 'use compression [zlib, none]', 'zlib')
-    .option('--textformat <string>', 'text format [plaintext, markdown] for the input', 'plaintext')
-    .option('-p, --password', 'prompt for password', false)
-    .option('-u, --url <string>', 'privateBin host', 'https://privatebin.net')
-    .option('-o, --output <string>', 'output format [text, json, yaml]', validateOutput, 'text')
-    .action(async (text, args) => {
-      if (args.burnafterreading && args.opendiscussion) {
-        throw new Error("You can't use --opendiscussion with --burnafterreading flag");
-      }
-
-      let password = '';
-      if (args.password) {
-        password = await readPassword();
-      }
-
-      const randomKey = crypto.getRandomValues(new Uint8Array(32));
-      const passPhrase = concatUint8Array(randomKey, stringToUint8Array(password));
-
-      const response = await sendCmdAction(text, passPhrase, args.url, {
-        expire: args.expire,
-        burnafterreading: args.burnafterreading ? 1 : 0,
-        opendiscussion: args.opendiscussion ? 1 : 0,
-        output: args.output,
-        textformat: args.textformat,
-        compression: args.compression,
-      });
-
-      const paste = formatResponse(response, args.url, randomKey);
-
-      switch (args.output) {
-        case 'json':
-          process.stdout.write(`${JSON.stringify(paste, null, 2)}\n`);
-          break;
-        case 'yaml':
-          process.stdout.write(`${YAML.stringify(paste)}\n`);
-          break;
-        default:
-          process.stdout.write(chalk`{bold pasteId:} ${paste.pasteId}\n`);
-          process.stdout.write(chalk`{bold pasteURL:} {greenBright ${paste.pasteURL}}\n`);
-          process.stdout.write(chalk`{bold deleteURL:} {gray ${paste.deleteURL}}\n`);
-      }
+    const response = await this.sendCmdAction(text, passPhrase, args.url, {
+      expire: args.expire,
+      burnafterreading: args.burnafterreading ? 1 : 0,
+      opendiscussion: args.opendiscussion ? 1 : 0,
+      output: args.output,
+      textformat: args.textformat,
+      compression: args.compression,
     });
 
-  return cmd;
+    const paste = this.formatResponse(response, args.url, randomKey);
+
+    switch (args.output) {
+      case 'json':
+        process.stdout.write(`${JSON.stringify(paste, null, 2)}\n`);
+        break;
+      case 'yaml':
+        process.stdout.write(`${YAML.stringify(paste)}\n`);
+        break;
+      default:
+        process.stdout.write(chalk`{bold pasteId:} ${paste.pasteId}\n`);
+        process.stdout.write(chalk`{bold pasteURL:} {greenBright ${paste.pasteURL}}\n`);
+        process.stdout.write(chalk`{bold deleteURL:} {gray ${paste.deleteURL}}\n`);
+    }
+  };
 }
